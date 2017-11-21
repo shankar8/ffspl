46e02fce9ac284bcbfbb36e378fdcb26f7f823cb
shankar8 <shankar.krishna1989@gmail.com>
2017-11-21 12:13:51 +0530

    added animation to header and icons

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
 $(function() {
 
 
-    var navigation = $(".navigation");
-
-console.log("haha");
+var navigation = $(".navigation");
 
 //------------------burger menu--------------------------------------//
 				
 $('.navicon').click(function() {
-    $('.navicon').toggleClass('blueburger').toggleClass('cross');
-    // headerlogo.toggleClass("whitelogo");
+    $('.navicon').toggleClass('blueburger').toggleClass('cross');    
     $('nav').toggleClass('navDark');
-    navigation.toggleClass('navToggle');
-    //$('li').toggleClass('menuButton');
-    
+    navigation.toggleClass('navToggle');    
 });
-//$('.cross').click(function() {
-    //$('.navicon').addClass('blueburger').removeClass('cross');                    
-//});
 
 $('ul li a').click(function() {
     console.log('navtttt');
-    //$('li').removeClass('menuButton');
     $('nav').removeClass('navDark');
     navigation.removeClass('navToggle');
     $('.navicon').addClass('blueburger').removeClass('cross');                       
@@ -32,6 +23,59 @@ $('.navigation.navToggle li a').click(function() {
 });
 
 
+
+//------------------scroll animations--------------------------------------//
+
+
+var heroTextTop = $('.hero-text h3').offset().top;
+var heroSectionBottom = $('.hero-section').height();
+
+    console.log(heroSectionBottom);
+
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+
+    if (wScroll > heroTextTop-220){
+        $('.tagline').addClass('gone');
+    } else{
+        $('.tagline').removeClass('gone');
+    }
+    // console.log(wScroll);
+    // console.log(heroTextHeight);
+    console.log(heroTextTop, heroSectionBottom);
+
+    if (wScroll > heroSectionBottom - 110) {
+        // console.log('tata');
+        $('.headerBg').addClass('moveUp');
+        $('.headerContent').addClass('moveUp');
+    }else{
+        $('.headerBg').removeClass('moveUp');
+        $('.headerContent').removeClass('moveUp');
+    }
+
+
+// sevice area animation
+    if(wScroll > $('.service-area').offset().top - ($(window).height() / 1.2)) {
+            
+
+            $('.service-icons').each(function(i){
+                console.log("harambe");
+              setTimeout(function(){
+                $('.service-img-box').eq(i).addClass('is-showing');
+              }, (700 * (Math.exp(i * 0.14))) - 700);
+            });
+        
+          }
+        
+
+
+
+
+
+
+
+
+});
 
 
 
